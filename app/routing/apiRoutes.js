cbbd4fbec9794a16024c8735ed045007ccea8e52
Adam Batimana <adambatimana@gmail.com>
2017-07-15 09:55:19 -0400

    score comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -34,7 +34,7 @@ app.get("/api/github", function(req,res){
          for (let i = 0; i < addFriend.scores.length; i++) {
              addFriend.scores[i] = parseInt(addFriend.scores[i])
              scoreInt = addFriend.scores[i];
-             console.log(scoreInt)
+            //  console.log(scoreInt)
          }
 
          //push information from .get to FRIENDS
@@ -45,49 +45,61 @@ app.get("/api/github", function(req,res){
         // //name selection
         // friendAddedName.push(friendData.name);
         // console.log(friendAddedName);
-        
+
         // all scores
         const matchScore = friendMatch.map(matchMe => matchMe.scores )
         console.log(matchScore)
 
-        //firstMatch score
-        const personOne = friendMatch[0].scores;
-        console.log(personOne)
-        //if person1 == person2 , match
 
+            // ===========================================
+            // =========  COMPARE SCORES  ================
+            // ===========================================
+
+            //pull information from freinds to compare scores then push match to modal on front endpoint
+
+            const matchOne = friendMatch[0].scores.reduce((a, b) => a + b, 0);
+            console.log(matchOne)
+
+            const matchTwo = friendMatch[1].scores.reduce((a, b) => a + b, 0);
+            console.log(matchTwo)
+
+            const matchThree = friendMatch[2].scores.reduce((a, b) => a + b, 0);
+            console.log(matchThree)
+
+            const matchFour = friendMatch[3].scores.reduce((a, b) => a + b, 0);
+            console.log(matchFour)
+
+            const userTotal = friendData[0].scores.reduce((a,b) => a +b, 0);
+            console.log(userTotal);
+
+
+          function checkMatch(friend,user){
+
+            const closeMatch = friend - user;
+            console.log(closeMatch)
+
+            if (friend === user)
+                {
+                   return true
+                }
+            else if (closeMatch > 0 && closeMatch < 2)
+                {
+                  return true
+                }
+            else
+                {
+                  return false
+                }
+          }//end checkMatch function
+
+          ;
+          console.log(checkMatch(matchTwo,userTotal));
+
+    });
+}//end export module
 
 
 //{Pseudo Code for finding match}
 //In  this example i = number of points in common
-        //if person1Score i > 5 = match
-        //else == no match
-
-
-    });
-
-
-
-    // ====================================
-    //            LOGIC comparison
-    // ====================================
-
-
-
-
-
-    // ===========================================
-    // =========  COMPARE SCORES  ================
-    // ===========================================
-
-    //pull information from freinds to compare scores then push match to modal on front endpoint
-
-
-
-
-
-
-
-
-
-
-}//end export module
+    //if person1Score i > 5 = match
+    //else == no match
